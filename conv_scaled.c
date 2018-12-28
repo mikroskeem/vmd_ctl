aadb8cd8f9236b76d215cbc2c1586334153a62d1
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-12-28 16:49:45 +0200

    Initial commit

---
+++ conv_scaled.c
@@ -0,0 +1,22 @@
+#include <util.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        fprintf(stderr, "Invalid usage\n");
+        return 1;
+    }
+
+    char *num = argv[1];
+    long long val = -1;
+
+    if(scan_scaled(num, &val) == 0) {
+        printf("%lld\n", val);
+        return 0;
+    } else {
+        fprintf(stderr, "Invalid value: %s\n", strerror(errno));
+        return 1;
+    }
+}
---
+++ vm.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use CGI;
+use Data::Dumper;
+use JSON;
+
+sub parse_usernum {
+    my ($num) = @_;
+
+    my $pid = open(my $proc, "-|", "/htdocs/vmm/cgi/conv_scaled $num") or die("wtf");
+    my $out = join "", <$proc>;
+    chomp $out;
+
+    return (length $out) ? $out + 0 : -1;
+}
+
+binmode STDOUT, ':utf8';
+my $q = CGI->new;
+
+print $q->header(-type => 'application/json', -charset => 'utf-8');
+
+my %res = ();
+
+my $vm_count = 0;
+my @vminfo = ();
+
+my $pid = open(my $proc, "-|", "doas /usr/sbin/vmctl status") or die("wtf");
+while(my $line = <$proc>) {
+    # Trim left side
+    $line =~ s/^\s+//;
+
+    my @sp = split(/\s+/, $line);
+    if ($sp[0] eq "ID") {
+        next
+    }
+    
+    # Increase vm counter
+    $vm_count++;
+
+    # Grab info
+    my %vm = (
+        "id" => $sp[0] + 0,
+        "name" => $sp[7],
+        "pid" => $sp[1] eq "-" ? undef : ($sp[1] + 0),
+        "running" => $sp[1] eq "-" ? JSON::false : JSON::true,
+        "memory" => {
+            "max" => parse_usernum($sp[3]),
+            "used" => $sp[4] eq "-" ? 0 : parse_usernum($sp[4])
+        },
+        "owner" => {}
+    );
+
+    ($vm{"owner"}{"user"}, $vm{"owner"}{"group"}) = split(/:/, $sp[6]);
+
+    # Append to vminfo array
+    push(@vminfo, \%vm);
+}
+
+# Format res
+$res{"vm_count"} = $vm_count;
+$res{"vms"} = \@vminfo;
+
+my $_json = JSON->new;
+$_json->canonical(1);
+print $_json->utf8->encode(\%res);
