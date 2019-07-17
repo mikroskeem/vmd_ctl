0ae471f7a43c44792a8c055613617f3bff0fd354
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-07-17 21:39:18 +0300

    Improve syntax

--- src/conv_scaled.c
+++ src/conv_scaled.c
@@ -8,7 +8,7 @@
 #include <errno.h>
 
 int main(int argc, char **argv) {
-    if(argc < 2) {
+    if (argc < 2) {
         fprintf(stderr, "Invalid usage\n");
         return 1;
     }
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
     char *num = argv[1];
     long long val = -1;
 
-    if(scan_scaled(num, &val) == 0) {
+    if (scan_scaled(num, &val) == 0) {
         printf("%lld\n", val);
         return 0;
     } else {
